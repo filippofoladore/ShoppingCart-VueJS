147388a84b83bde9b554eb2ae79479223fd8807a
Filippo Foladore <filippo.foladore@gmail.com>
2020-09-30 10:34:39 +0200

    Change database whitelist.

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -11,24 +11,39 @@ orderRouter.get('/:id', async (req, res) => {
 })
 
 orderRouter.get('/detail/:id', async (req, res) => {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    const client = await connect();
+    // const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     data = client.db('CheckoutDB').collection('orders')
     res.send(await data.findOne({ "user": new mongodb.ObjectID(req.params.id) }))
 })
 
 
 orderRouter.get('/article/:id', async (req, res) => {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    const client = await connect();
+    // const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     data = client.db('CheckoutDB').collection('articles')
     res.send(await data.findOne({ "_id": new mongodb.ObjectID(req.params.id) }))
 })
 
 orderRouter.get('/complete/:id', async (req, res) => {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    const client = await connect();
+    // const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     data = client.db('CheckoutDB').collection('orders')
     let iden = { "user": new mongodb.ObjectID(req.params.id) }
-    let neaa = {$set: {status: 'completed'}}
+    let neaa = { $set: { status: 'completed' } }
     res.send(await data.updateOne(iden, neaa))
 })
+
+async function connect() {
+    return client = await mongodb.MongoClient.connect(
+        'mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true, useUnifiedTopology: true
+        }
+    );
+}
+
+
+
 
 module.exports = orderRouter
