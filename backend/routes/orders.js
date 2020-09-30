9d2c26aa4826a344b558715d892091f56713e250
Filippo Foladore <filippo.foladore@gmail.com>
2020-09-30 10:48:18 +0200

    Refactor

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,16 +3,8 @@ const mongodb = require('mongodb')
 
 const orderRouter = express.Router()
 
-//get user for name display
-orderRouter.get('/:id', async (req, res) => {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    data = client.db('CheckoutDB').collection('users')
-    res.send(await data.findOne({ "_id": new mongodb.ObjectID(req.params.id) }))
-})
-
 orderRouter.get('/detail/:id', async (req, res) => {
     const client = await connect();
-    // const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     data = client.db('CheckoutDB').collection('orders')
     res.send(await data.findOne({ "user": new mongodb.ObjectID(req.params.id) }))
 })
@@ -20,26 +12,22 @@ orderRouter.get('/detail/:id', async (req, res) => {
 
 orderRouter.get('/article/:id', async (req, res) => {
     const client = await connect();
-    // const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     data = client.db('CheckoutDB').collection('articles')
     res.send(await data.findOne({ "_id": new mongodb.ObjectID(req.params.id) }))
 })
 
 orderRouter.get('/complete/:id', async (req, res) => {
     const client = await connect();
-    // const client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     data = client.db('CheckoutDB').collection('orders')
-    let iden = { "user": new mongodb.ObjectID(req.params.id) }
-    let neaa = { $set: { status: 'completed' } }
-    res.send(await data.updateOne(iden, neaa))
+    let query = { "user": new mongodb.ObjectID(req.params.id) }
+    let changes = { $set: { status: 'completed' } }
+    res.send(await data.updateOne(query, changes))
 })
 
 async function connect() {
-    return client = await mongodb.MongoClient.connect(
-        'mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority',
-        {
-            useNewUrlParser: true, useUnifiedTopology: true
-        }
+    return client = await mongodb.MongoClient.connect('mongodb+srv://filippoDB:filippoDB@cluster0.ghzdf.mongodb.net/atroosdb?retryWrites=true&w=majority', {
+        useNewUrlParser: true, useUnifiedTopology: true
+    }
     );
 }
 
